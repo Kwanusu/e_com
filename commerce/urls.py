41fb84508110c90dc567f12b273f63695fbed183
Kwanusu <kwanusujoseph@gmail.com>
2024-07-11 21:18:15 +0300

    additions

--- commerce/urls.py
+++ commerce/urls.py
@@ -15,11 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.db import router
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('online_shop.urls')),
+    path('', include('Admin.urls')),
+    
 ]
 urlpatterns += staticfiles_urlpatterns()
