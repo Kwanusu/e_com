c17b32016016cc39027bf151d4a4e93c1727b8ea
Kwanusu <kwanusujoseph@gmail.com>
2024-12-03 17:16:10 +0300

    changed the db url

--- commerce/settings.py
+++ commerce/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = config('SECRET_KEY')
 BASE_URL = 'https://api.stripe.com'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
